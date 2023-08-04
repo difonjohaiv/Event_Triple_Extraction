1f451a3965ff6012a11a6c1caf713c6078e2edc3
snailshaw <snailshaw@126.com>
2023-08-04 13:13:49 +0800

    0

---
+++ main.py
@@ -0,0 +1,10 @@
+from triple_extraction_ltp import TripleExtraction
+
+content1 = '李克强总理今天来我家了,我感到非常荣幸'
+content2 = ''' 以色列国防军20日对加沙地带实施轰炸，造成3名巴勒斯坦武装人员死亡。此外，巴勒斯坦人与以色列士兵当天在加沙地带与以交界地区发生冲突，一名巴勒斯坦人被打死。当天的冲突还造成210名巴勒斯坦人受伤。当天，数千名巴勒斯坦人在加沙地带边境地区继续“回归大游行”抗议活动。部分示威者燃烧轮胎，并向以军投掷石块、燃烧瓶等，驻守边境的以军士兵向示威人群发射催泪瓦斯并开枪射击。'''
+
+extractor = TripleExtraction()
+for text in [content1, content2]:
+    print(text)
+    res = extractor.triples_main(text)
+    print(res)
--- triple_extraction_ltp.py
+++ triple_extraction_ltp.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 """
 采用ltp进行三元组抽取
 根据https://github.com/liuhuanyong/EventTriplesExtraction改编，
@@ -14,21 +13,26 @@ class LtpParser:
         self.ltp = LTP()
 
     '''语义角色标注'''
+
     def format_labelrole(self, hidden):
         roles = self.ltp.srl(hidden, keep_empty=False)
         roles_dict = {}
         for role in roles[0]:
-            roles_dict[role[0]] = {arg[0]: [arg[0], arg[1], arg[2]] for arg in role[1]}
+            roles_dict[role[0]] = {
+                arg[0]: [arg[0], arg[1], arg[2]]
+                for arg in role[1]
+            }
         return roles_dict
 
     '''句法分析---为句子中的每个词语维护一个保存句法依存儿子节点的字典'''
+
     def build_parse_child_dict(self, words, postags, arcs):
         child_dict_list = []
         format_parse_list = []
         for index in range(len(words)):
             child_dict = dict()
             for arc_index in range(len(arcs)):
-                if arcs[arc_index][1] == index+1:   #arcs的索引从1开始
+                if arcs[arc_index][1] == index + 1:  # arcs的索引从1开始
                     if arcs[arc_index][2] in child_dict:
                         child_dict[arcs[arc_index][2]].append(arc_index)
                     else:
@@ -37,21 +41,27 @@ class LtpParser:
             child_dict_list.append(child_dict)
         rely_id = [arc[1] for arc in arcs]  # 提取依存父节点id
         relation = [arc[2] for arc in arcs]  # 提取依存关系
-        heads = ['Root' if id == 0 else words[id - 1] for id in rely_id]  # 匹配依存父节点词语
+        heads = ['Root' if id == 0 else words[id - 1]
+                 for id in rely_id]  # 匹配依存父节点词语
         for i in range(len(words)):
             # ['ATT', '李克强', 0, 'nh', '总理', 1, 'n']
-            a = [relation[i], words[i], i, postags[i], heads[i], rely_id[i]-1, postags[rely_id[i]-1]]
+            a = [
+                relation[i], words[i], i, postags[i], heads[i], rely_id[i] - 1,
+                postags[rely_id[i] - 1]
+            ]
             format_parse_list.append(a)
         return child_dict_list, format_parse_list
 
     '''parser主函数'''
+
     def parser_main(self, sentence):
         words, hidden = self.ltp.seg([sentence])
         postags = self.ltp.pos(hidden)
         arcs = self.ltp.dep(hidden)
         words, postags, arcs = words[0], postags[0], arcs[0]
         # print(words, '\n', postags, '\n', arcs)
-        child_dict_list, format_parse_list = self.build_parse_child_dict(words, postags, arcs)
+        child_dict_list, format_parse_list = self.build_parse_child_dict(
+            words, postags, arcs)
         roles_dict = self.format_labelrole(hidden)
         return words, postags, child_dict_list, roles_dict, format_parse_list
 
@@ -61,23 +71,34 @@ class TripleExtraction():
         self.parser = LtpParser()
 
     '''文章分句处理, 切分长句，冒号，分号，感叹号等做切分标识'''
+
     def split_sents(self, content):
         return self.parser.ltp.sent_split([content])
 
     '''利用语义角色标注,直接获取主谓宾三元组,基于A0,A1,A2'''
+
     def ruler1(self, words, postags, roles_dict, role_index):
         v = words[role_index]
         role_info = roles_dict[role_index]
         if 'A0' in role_info.keys() and 'A1' in role_info.keys():
-            s = ''.join([words[word_index] for word_index in range(role_info['A0'][1], role_info['A0'][2] + 1) if
-                         postags[word_index][0] not in ['w', 'u', 'x'] and words[word_index]])
-            o = ''.join([words[word_index] for word_index in range(role_info['A1'][1], role_info['A1'][2] + 1) if
-                         postags[word_index][0] not in ['w', 'u', 'x'] and words[word_index]])
+            s = ''.join([
+                words[word_index] for word_index in range(
+                    role_info['A0'][1], role_info['A0'][2] + 1)
+                if postags[word_index][0] not in ['w', 'u', 'x']
+                and words[word_index]
+            ])
+            o = ''.join([
+                words[word_index] for word_index in range(
+                    role_info['A1'][1], role_info['A1'][2] + 1)
+                if postags[word_index][0] not in ['w', 'u', 'x']
+                and words[word_index]
+            ])
             if s and o:
                 return '1', [s, v, o]
         return '4', []
 
     '''三元组抽取主函数'''
+
     def ruler2(self, words, postags, child_dict_list, arcs, roles_dict):
         svos = []
         for index in range(len(postags)):
@@ -97,8 +118,10 @@ class TripleExtraction():
                     # 主谓宾
                     if 'SBV' in child_dict and 'VOB' in child_dict:
                         r = words[index]
-                        e1 = self.complete_e(words, postags, child_dict_list, child_dict['SBV'][0])
-                        e2 = self.complete_e(words, postags, child_dict_list, child_dict['VOB'][0])
+                        e1 = self.complete_e(words, postags, child_dict_list,
+                                             child_dict['SBV'][0])
+                        e2 = self.complete_e(words, postags, child_dict_list,
+                                             child_dict['VOB'][0])
                         svos.append([e1, r, e2])
 
                     # 定语后置，动宾关系
@@ -106,9 +129,12 @@ class TripleExtraction():
                     head = arcs[index][2]
                     if relation == 'ATT':
                         if 'VOB' in child_dict:
-                            e1 = self.complete_e(words, postags, child_dict_list, head - 1)
+                            e1 = self.complete_e(words, postags,
+                                                 child_dict_list, head - 1)
                             r = words[index]
-                            e2 = self.complete_e(words, postags, child_dict_list, child_dict['VOB'][0])
+                            e2 = self.complete_e(words, postags,
+                                                 child_dict_list,
+                                                 child_dict['VOB'][0])
                             temp_string = r + e2
                             if temp_string == e1[:len(temp_string)]:
                                 e1 = e1[len(temp_string):]
@@ -116,11 +142,14 @@ class TripleExtraction():
                                 svos.append([e1, r, e2])
                     # 含有介宾关系的主谓动补关系
                     if 'SBV' in child_dict and 'CMP' in child_dict:
-                        e1 = self.complete_e(words, postags, child_dict_list, child_dict['SBV'][0])
+                        e1 = self.complete_e(words, postags, child_dict_list,
+                                             child_dict['SBV'][0])
                         cmp_index = child_dict['CMP'][0]
                         r = words[index] + words[cmp_index]
                         if 'POB' in child_dict_list[cmp_index]:
-                            e2 = self.complete_e(words, postags, child_dict_list, child_dict_list[cmp_index]['POB'][0])
+                            e2 = self.complete_e(
+                                words, postags, child_dict_list,
+                                child_dict_list[cmp_index]['POB'][0])
                             svos.append([e1, r, e2])
         return svos
 
@@ -131,13 +160,16 @@ class TripleExtraction():
         prefix = ''
         if 'ATT' in child_dict:
             for i in range(len(child_dict['ATT'])):
-                prefix += self.complete_e(words, postags, child_dict_list, child_dict['ATT'][i])
+                prefix += self.complete_e(words, postags, child_dict_list,
+                                          child_dict['ATT'][i])
         postfix = ''
         if postags[word_index] == 'v':
             if 'VOB' in child_dict:
-                postfix += self.complete_e(words, postags, child_dict_list, child_dict['VOB'][0])
+                postfix += self.complete_e(words, postags, child_dict_list,
+                                           child_dict['VOB'][0])
             if 'SBV' in child_dict:
-                prefix = self.complete_e(words, postags, child_dict_list, child_dict['SBV'][0]) + prefix
+                prefix = self.complete_e(words, postags, child_dict_list,
+                                         child_dict['SBV'][0]) + prefix
 
         return prefix + words[word_index] + postfix
 
@@ -145,10 +177,12 @@ class TripleExtraction():
 
     def triples_main(self, content):
         sentences = self.split_sents(content)
-        sentences = [sent for sent in sentences if len(sent.strip())>5]
+        sentences = [sent for sent in sentences if len(sent.strip()) > 5]
         svos = []
         for sentence in sentences:
-            words, postags, child_dict_list, roles_dict, arcs = self.parser.parser_main(sentence)
-            svo = self.ruler2(words, postags, child_dict_list, arcs, roles_dict)
+            words, postags, child_dict_list, roles_dict, arcs = self.parser.parser_main(
+                sentence)
+            svo = self.ruler2(words, postags, child_dict_list, arcs,
+                              roles_dict)
             svos += svo
         return svos
